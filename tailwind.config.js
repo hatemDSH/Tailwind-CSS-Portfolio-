09041e44451a1b64b4ce3e00b926753aaec200df
hatemDSH <hatderza@gmail.com>
2024-09-10 03:54:14 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    
+  },
+  plugins: [],
+  
+}
+/*
+@tailwind base;
+@tailwind components;
+@tailwind utilities;*/
